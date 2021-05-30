bbe83e395a62a2916840559a4b430c18df45f75c
Iago Rodrigues <yagomello16@gmail.com>
2021-05-30 10:16:32 -0300

    init project

---
+++ src/main/java/dev/iagorodrigues/clientapi/DsbootcampJavaFirstTaskApplication.java
@@ -0,0 +1,13 @@
+package dev.iagorodrigues.clientapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsbootcampJavaFirstTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsbootcampJavaFirstTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/iagorodrigues/clientapi/DsbootcampJavaFirstTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.iagorodrigues.clientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsbootcampJavaFirstTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
