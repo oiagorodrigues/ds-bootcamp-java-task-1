9f73a899bc77d6d658d6ee3ce09c9b07d8c01b1d
Iago Rodrigues <yagomello16@gmail.com>
2021-05-30 21:09:08 -0300

    feat: add custom exceptions and standard error class

---
+++ src/main/java/dev/iagorodrigues/clientapi/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package dev.iagorodrigues.clientapi.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
